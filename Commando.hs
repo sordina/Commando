fc8bf2de7ec4dee62462cf25f24c586814d16990
Lyndon Maydwell <lyndon@sordina.net>
2013-06-25 15:12:07 +1000

    Using optparse for option parsing

--- Commando.hs
+++ Commando.hs
@@ -1,60 +1,99 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns      #-}
 
 module Main where
 
 import Prelude hiding            (FilePath)
 import Control.Monad             (void, when)
 import System.Process            (rawSystem, runCommand, runInteractiveCommand)
-import System.Environment        (getArgs)
 import System.FSNotify           (startManager, watchTree, stopManager)
 import Filesystem                (getWorkingDirectory)
 import Filesystem.Path.CurrentOS (FilePath, fromText)
 import Data.Text                 (pack)
-import System.IO                 (hPutStrLn, hGetContents, hSetBinaryMode)
-import GHC.IO.Handle             (hClose)
+import System.IO                 (hPutStrLn, hGetContents, hSetBuffering, BufferMode(..), stdout, hIsEOF)
+import GHC.IO.Handle             (hClose, hFlush)
+import GHC.IO.Handle.Types       (Handle)
+import System.Process.Internals  (ProcessHandle)
+import Control.Applicative       ((<$>), (<*>))
+import Data.Monoid               (mempty, (<>))
 
-data Mode = StandAlone
-          | Argument
-          | StdIn
+import qualified Options.Applicative as O
+
+type RunningProcess = (Handle, Handle, Handle, ProcessHandle)
+
+data Options = Options { command'   :: String
+                       , quiet      :: Bool
+                       , consumer   :: Bool
+                       , stdin      :: Bool
+                       , persist    :: Bool
+                       , directory' :: Maybe String
+                       } deriving Show
+
+options :: O.Parser Options
+options = Options <$>             O.argument O.str ( O.metavar "COMMAND"              <> O.help "Command run on events")
+                  <*>             O.switch         ( O.short 'q' <> O.long "quiet"    <> O.help "Hide non-essential output")
+                  <*>             O.switch         ( O.short 'c' <> O.long "consumer" <> O.help "Pass events as argument to command")
+                  <*>             O.switch         ( O.short 'i' <> O.long "stdin"    <> O.help "Pipe events to command")
+                  <*>             O.switch         ( O.short 'p' <> O.long "persist"  <> O.help "Pipe events to persistent command")
+                  <*> O.optional (O.argument O.str ( O.metavar "DIRECTORY"            <> O.help "Directory to monitor" ))
+
+parser :: O.ParserInfo Options
+parser = O.info (O.helper <*> options) mempty
 
 main :: IO ()
-main = getArgs >>= options False StandAlone
+main = O.execParser parser >>= command
 
-options :: Bool -> Mode -> [String] -> IO ()
-options _ _ ("-h"        : _) = help
-options _ _ ("--help"    : _) = help
-options _ c ("-s"        :xs) = options True c        xs
-options _ c ("--silent"  :xs) = options True c        xs
-options s _ ("-c"        :xs) = options s    Argument xs
-options s _ ("--consumer":xs) = options s    Argument xs
-options s _ ("-i"        :xs) = options s    StdIn    xs
-options s _ ("--stdin"   :xs) = options s    StdIn    xs
-options s c ("--"        :xs) = command s    c        xs
-options s c xs                = command s    c        xs
+command :: Options -> IO ()
+command o@(directory' -> Just d) = start o (fromText $ pack d)
+command o                        = getWorkingDirectory >>= start o
 
-command :: Bool -> Mode -> [String] -> IO ()
-command s c [cmd]      = getWorkingDirectory >>= start s c cmd
-command s c [cmd, dir] =                         start s c cmd (fromText $ pack $ dir)
-command _ _ _          = help
-
-help :: IO ()
-help = putStrLn "Usage: commando [--help | -h] [--silent | -s] [--consumer | -c] [--stdin | -i] [--] <command> [directory]"
-
-start :: Bool -> Mode -> String -> FilePath -> IO ()
-start silent mode cmd dir = do
+start :: Options -> FilePath -> IO ()
+start o dir = do
   man <- startManager
-  when (not silent) $ putStrLn "press retrun to stop"
+  rc  <- if persist o then Just <$> startPipe (command' o)
+                      else return Nothing
+
+  when (not $ quiet o) $ putStrLn "press retrun to stop"
+
+  let cmd = command' o
+
   void $ watchTree man dir (const True)
-       $ case mode of StandAlone -> const $ void $ runCommand cmd
-                      Argument   -> void . rawSystem cmd . return . show
-                      StdIn      -> void . pipe cmd . show
+       $ case (consumer o, stdin o || persist o )
+           of (True      , _    ) -> void . rawSystem cmd . return . show
+              (_         , True ) -> void . pipe rc cmd . show
+              (_         , _    ) -> const $ void $ runCommand cmd
+
   void $ getLine
+
   void $ stopManager man
 
-pipe :: String -> String -> IO ()
-pipe cmd param = do
-  (hStdIn, stdOut, _stderr, _process) <- runInteractiveCommand cmd
-  void $ hSetBinaryMode hStdIn False
-  void $ hPutStrLn hStdIn param
-  void $ hClose hStdIn
-  void $ hGetContents stdOut >>= putStr
+  case rc of Just p -> pipeClose p
+             _      -> return ()
+
+startPipe :: String -> IO RunningProcess
+startPipe cmd = do
+  rc@(hStdIn,_,_,_) <- runInteractiveCommand cmd
+  hSetBuffering hStdIn NoBuffering
+  return rc
+
+pipe :: Maybe RunningProcess -> String -> String -> IO ()
+pipe Nothing   cmd arg = startPipe cmd >>= pipeRunning arg >>= pipeClose
+pipe (Just rp) _   arg = void $ pipeRunning arg rp
+
+pipeClose :: RunningProcess -> IO ()
+pipeClose (hStdIn, _, _, _) = hClose hStdIn
+
+pipeRunning :: String -> RunningProcess -> IO RunningProcess
+pipeRunning param rc@(hStdIn, hStdOut, _stderr, _process) = do
+
+  hPutStrLn hStdIn param
+  hFlush hStdIn
+
+  eof <- hIsEOF hStdOut
+
+  when (not eof) $ do
+
+    hGetContents hStdOut >>= putStr
+    hFlush stdout
+
+  return rc
