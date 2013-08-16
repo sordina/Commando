c3b2f29109739a94bd102cc1b77e13b80d3638f0
Lyndon Maydwell <lyndon@sordina.net>
2013-08-16 16:48:24 +1000

    Line Buffer

--- Commando.hs
+++ Commando.hs
@@ -6,11 +6,13 @@ import Data.Monoid         (mempty)
 import Control.Monad       (when)
 import System.Commando     (commando, options, quiet)
 import Options.Applicative (info, helper, execParser, (<*>))
+import System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdout LineBuffering
+  hSetBuffering stderr LineBuffering
+
   opts <- execParser (info (helper <*> options) mempty)
-
-  when (not $ quiet opts) $ putStrLn "press return to stop"
-
+  when (not $ quiet opts) $ putStrLn "press return to quit"
   commando opts >>= mapM_ putStr
